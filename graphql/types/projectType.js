69b061298a88aef50a9cb2c71f6667bc33f18a9a
madalinioana <madalinioana@icloud.com>
2026-01-05 22:39:57 +0200

    Add GraphQL types for Project and Repository

---
+++ graphql/types/projectType.js
@@ -0,0 +1,51 @@
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } = require('graphql');
+const RepositoryType = require('./repositoryType');
+const UserType = require('./userType'); 
+const db = require('../../models');
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    projectID: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    repositoryID: { type: GraphQLInt },
+    
+    repository: {
+      type: RepositoryType,
+      resolve: async (project) => {
+        if (project.repository) {
+          return project.repository;
+        }
+        
+        if (project.getRepository) {
+            return await project.getRepository();
+        }
+        
+        return null;
+      },
+    },
+
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: async (project) => {
+        if (project.users && project.users.length > 0) {
+            return project.users;
+        }
+
+        if (project.getUsers) {
+            return await project.getUsers({
+                include: [
+                    { model: db.Team, as: 'team' },
+                    { model: db.Position, as: 'position' },
+                    { model: db.Role, as: 'roles' }
+                ]
+            });
+        }
+        return [];
+      },
+    },
+  }),
+});
+
+module.exports = ProjectType;
